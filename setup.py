7c345c53290f29a4299cfcd981bd55cc86257572
Midhun Manohar <midhunmanohar25@gmail.com>
2026-04-17 19:08:48 +0530

    Mlops new project start

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is about ml ops that how different components works together',
+    author='Midhun Manohar',
+    license='',
+)
